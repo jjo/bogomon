a7b3cee00a2574b66237e4498c30c952ccb62d40
JuanJo Ciarlante <juanjosec@gmail.com>
2011-11-10 10:00:17 -0800

    added threads cron, more STATS

--- web/jjo.fcgi.py
+++ web/jjo.fcgi.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 """Implements foo"""
 
-import os, re, time, rrdtool
+import os, re, time, rrdtool, thread
 from flup.server.fcgi import WSGIServer
 
 def _read_cpu():
@@ -12,6 +12,7 @@ def _read_cpu():
   cpu_array = re.match('(cpu)\W+(\d+) (\d+) (\d+) (\d+)', cpu_string).groups()
   user, user_nice, system = [int(x) for x in cpu_array[1:4]]
   used = user+user_nice+system
+  STATS['cpu_latest'] = used
   return ( time.time(), int(used*1e3/JIFFIES))
 
 def stat_cpu_txt(_, txt_prefix):
@@ -21,6 +22,7 @@ def stat_cpu_txt(_, txt_prefix):
  
 def save_cpu_sto(_, txt_prefix):
   """short cut to update local rrd"""
+  STATS['cpu_stos'] = STATS['cpu_stos'] + 1
   cpu = _read_cpu()
   rrdtool.update('../rrdtool/jjo.rrd', '-t', 'cpurate',
       '%s:%s' % (cpu[0], cpu[1]))
@@ -28,6 +30,8 @@ def save_cpu_sto(_, txt_prefix):
 
 def save_cpu_png(_, txt_prefix):
   """return a png"""
+  STATS['cpu_pngs'] = STATS['cpu_pngs'] + 1
+  STATS['png_timestamp'] = time.time()
   rrdtool.graph('../rrdtool/jjo.png',
       'DEF:cpu=../rrdtool/jjo.rrd:cpurate:AVERAGE',
       'LINE1:cpu#ff0000:cpu')
@@ -50,17 +54,29 @@ def stats_html(_, txt_prefix):
     <head><title>%(title)s</title></head>
     <body>
       hits: %(hits)s<br>
+      crons: %(crons)s<br>
+      cpu_stos: %(cpu_stos)s<br>
+      cpu_pngs: %(cpu_pngs)s<br>
+      cpu_latest: %(cpu_latest)s<br>
       <img src="%(img)s"/>
     </body>
   </html>
   """ % {
       'title': txt_prefix,
       'img': "/r/stat/cpu/png",
-      'hits': STATS['hits']
+      'hits': STATS['hits'],
+      'crons': STATS['crons'],
+      'cpu_stos': STATS['cpu_stos'],
+      'cpu_pngs': STATS['cpu_pngs'],
+      'cpu_latest' : STATS['cpu_latest'],
   }
 
 STATS = {
     'hits': 0,
+    'crons': 0,
+    'cpu_stos': 0,
+    'cpu_pngs': 0,
+    'cpu_latest': 0,
     'png_timestamp': time.time(),
 }
 
@@ -71,6 +87,13 @@ PATH_REGISTRY = {
     '/w/save/cpu/sto':  ["text/plain", save_cpu_sto ],
     '/w/save/cpu/png':  ["text/plain", save_cpu_png ],
 }
+
+def _cron_save_cpu():
+  """ Periodically save cpu state """
+  while True:
+    STATS['crons'] = STATS['crons'] + 1
+    save_cpu_sto([], "")
+    time.sleep(10)
 
 def app(environ, start_response):
   """main fcgi entry point"""
@@ -84,5 +107,7 @@ def app(environ, start_response):
     start_response('404 Not found', [('Content-Type', 'text/plain')])
     yield "Not found\n"
 
+## main()
 JIFFIES = os.sysconf(os.sysconf_names['SC_CLK_TCK'])
+thread.start_new_thread(_cron_save_cpu, ())
 WSGIServer(app).run()
