cc0d1be8fba13f733d2457face8b53966f6946f2
JuanJo Ciarlante <juanjosec@gmail.com>
2011-11-09 22:35:57 -0800

    fcgi+python+rrdtool

--- web/jjo.fcgi.py
+++ web/jjo.fcgi.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 """Implements foo"""
 
-import os, re, time
+import os, re, time, rrdtool
 from flup.server.fcgi import WSGIServer
 
 def get_cpu(_):
@@ -12,25 +12,44 @@ def get_cpu(_):
   if cpu_array:
     user, nice, system = [int(x) for x in cpu_array[1:4]]
     used = user+nice+system
-    return "%d %d" % ( time.time(), int(used*1e3/JIFFIES))
-  return None
+    return ('text/plain',
+        '%d %d' % ( time.time(), int(used*1e3/JIFFIES)))
   
+def sample_local(_):
+  """short cut to update local rrd"""
+  cpu = get_cpu(_)[1].split(" ")
+  rrdtool.update('../rrdtool/jjo.rrd', '-t', 'cpurate',
+      '%s:%s' % (cpu[0], cpu[1]))
+  return ('text/plain', '%s saved' % cpu)
+
+def graph_cpu(_):
+  """return a png"""
+  rrdtool.graph('../rrdtool/jjo.png',
+      'DEF:cpu=../rrdtool/jjo.rrd:cpurate:AVERAGE',
+      'LINE1:cpu#ff0000:cpu')
+  return ('text/plain', 'graphed')
+
+def graph_cpu_png(_):
+  """return a png"""
+  return ('image/png',
+      open('../rrdtool/jjo.png').read())
+
+
 PATH_REGISTRY = {
-    '/stat': '',
     '/stat/cpu': get_cpu,
+    '/sample/local': sample_local,
+    '/graph/cpu': graph_cpu,
+    '/graph/cpu/png': graph_cpu_png,
     }
 
 def app(environ, start_response):
   """main fcgi entry point"""
-  start_response('200 OK', [('Content-Type', 'text/plain')])
   func = PATH_REGISTRY.get(environ['PATH_INFO'])
   if func is not None:
     if callable(func):
-      val = func(environ)
-    else:
-      val = func
-    yield '%s %s %s\n' % (
-        environ['SERVER_NAME'], environ['PATH_INFO'], val)
+      content_type, val = func(environ)
+      start_response('200 OK', [('Content-Type', content_type)])
+      yield val
 
 JIFFIES = os.sysconf(os.sysconf_names['SC_CLK_TCK'])
 WSGIServer(app).run()
