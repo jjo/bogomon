757a70bc0519445156e1cadb6152952fbee896ee
JuanJo Ciarlante <juanjosec@gmail.com>
2011-11-10 23:51:53 -0800

    note about web/run-bogomon.sh no-more

--- web/run-bogomon.sh
+++ web/run-bogomon.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -x
-spawn-fcgi -n -P ./var/lib/spawn-fcgi.pid -d $PWD/web -f $PWD/webbogomon.fcgi.py -a 127.0.0.1 -p ${1:?missing port}
+### DEPRECATED: see ../Makefile for: make run ######
+#   spawn-fcgi -n -P ./var/lib/spawn-fcgi.pid -d $PWD/web -f $PWD/webbogomon.fcgi.py -a 127.0.0.1 -p ${1:?missing port}
