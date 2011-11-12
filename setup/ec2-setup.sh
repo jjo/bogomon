0bcdbe1742ccd2be7ceafd7e2f5d660afc07508b
JuanJo Ciarlante <juanjosec@gmail.com>
2011-11-11 23:47:10 -0800

    cleanup, init.d fixes

--- setup/ec2-setup.sh
+++ setup/ec2-setup.sh
@@ -4,11 +4,12 @@ sudo apt-get -y install $PKGS
 id -u bogomon || sudo useradd bogomon || exit 1
 sudo mkdir -p /usr/lib/bogomon /var/lib/bogomon /var/run/bogomon
 REPO=https://raw.github.com/jjo/bogomon/master
-for f in etc/nginx/sites-available/bogomon usr/lib/bogomon/bogomon.fcgi.py etc/init.d/spawn-fcgi;do
-	sudo wget -O /$f $REPO/$f || exit 1
+for f in /etc/nginx/sites-available/bogomon /usr/lib/bogomon/bogomon.fcgi.py /etc/init.d/spawn-fcgi;do
+	sudo wget -O $f $REPO$f || exit 1
+	case $f in /etc/init.d/*|/usr/lib/*) sudo chmod +x $f;;esac
 done
 sudo ln -sf ../sites-available/bogomon /etc/nginx/sites-enabled/bogomon
-sudo chmod +x /etc/init.d/spawn-fcgi
+sudo chown -R bogomon /var/lib/bogomon
 sudo /etc/init.d/spawn-fcgi restart
 sudo /etc/init.d/nginx configtest || exit 1
 sudo /etc/init.d/nginx status && sudo /etc/init.d/nginx start || sudo /etc/init.d/nginx restart
