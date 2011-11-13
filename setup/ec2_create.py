0115cd9dbcddceae8a23950a74bd22cb735a01a7
JuanJo Ciarlante <juanjosec@gmail.com>
2011-11-13 01:04:04 -0800

    OOP-ify ec2_setup.py

--- setup/ec2_create.py
+++ setup/ec2_create.py
@@ -14,68 +14,80 @@ IMG = 'ami-6936fb00' ## ubuntu 10.04 32bit, small, us-east-1
 BOGOMON_PORT = 8000  ## only used for the final "informational" message
 BRANCH = 'master'    ## potentially allows for e.g. 'staging', 'experimental'
 
-def start(ec2_img, ec2_keypair):
-  """ Connect to EC2, spawn an instance from ec2_img, attaching
-      ec2_keypair to it """
-  print 'Starting EC2 with image {0}'.format(ec2_img)
-  ec2_conn = boto.connect_ec2()
-  images = ec2_conn.get_all_images(image_ids=[ec2_img])
-  reserv = images[0].run(1, 1, ec2_keypair)
-  instance = reserv.instances[0]
-  time.sleep(10)
-  while not instance.update() == 'running':
-    time.sleep(5)
-  while not instance.dns_name:
-    instance.update()
-    time.sleep(1)
-  print 'Started the hostname: {0}, instance: {1}'.format(
-      instance.dns_name, instance)
-  return instance
+class EC2bogo():
+  """ Encapsulates EC2 instance creation and cmd running """
+  def __init__(self, ec2_img, ec2_keypair, ssh_privk, branch):
+    self.ec2_img = ec2_img
+    self.ec2_keypair = ec2_keypair
+    if ssh_privk and not os.path.isfile(ssh_privk):
+      sys.exit('Error: file %s doesnt exist' % ssh_privk)
+    self.ssh_privk = ssh_privk
+    self.branch = branch
+    self.instance = None
 
-def run(ssh_pubk, host, cmd):
-  """ Run: ssh ubunbu@... at just created host, to complete its setup """
-  for _ in xrange(30):
-    try:
-      telnetlib.Telnet(host, 22)
-      break
-    except socket.error:
+  def start(self):
+    """ Connect to EC2, spawn an instance from ec2_img, attaching
+        ec2_keypair to it """
+    print 'Starting EC2 with image {0}'.format(self.ec2_img)
+    ec2_conn = boto.connect_ec2()
+    images = ec2_conn.get_all_images(image_ids=[self.ec2_img])
+    reserv = images[0].run(1, 1, self.ec2_keypair)
+    self.instance = reserv.instances[0]
+    time.sleep(10)
+    while not self.instance.update() == 'running':
       time.sleep(5)
-  # sshd already listening, but allow some time for key propagation
-  time.sleep(5)
-  if ssh_pubk:
-    ssh_pubk = "-i %s" % ssh_pubk
-  retval = os.system('set -x;ssh -v %s ubuntu@%s "%s"' % (
-    ssh_pubk, host, cmd))
-  if retval != 0:
-    raise SystemExit
+    while not self.instance.dns_name:
+      self.instance.update()
+      time.sleep(1)
+    print 'Started the hostname: {0}, instance: {1}'.format(
+        self.instance.dns_name, self.instance)
+
+  def run(self, cmd):
+    """ Run: ssh ubunbu@... at just created host, to complete its setup """
+    for _ in xrange(30):
+      try:
+        telnetlib.Telnet(self.instance.dns_name, 22)
+        break
+      except socket.error:
+        time.sleep(5)
+    # sshd already listening, but allow some time for key propagation
+    time.sleep(5)
+    if self.ssh_privk:
+      ssh_i = '-i %s' % self.ssh_privk
+    else:
+      ssh_i = ''
+
+    retval = os.system('set -x;ssh -v %s ubuntu@%s "%s"' % (
+      ssh_i, self.instance.dns_name, cmd))
+
+    if retval != 0:
+      print 'Failed ssh setup, destroying {0}'.format(self.instance)
+      self.instance.stop()
+      raise SystemExit  ### 
 
 def main():
   """ main(), what else """
   # Override default args with argv[1:] ,
   # also IMO easier for validation instead of len(sys.argv)
-  args = [None, None, BRANCH]
+  args = [None, None, BRANCH, IMG]
   for i, val in enumerate(sys.argv[1:]):
     args[i] = val
   if args[0] is None:
     sys.exit('Usage: %s <ec2_keypair> [ssh_priv_key_filepath] [branch]\n')
 
-  ec2_keypair, ec2_sshpubk, branch = args
-  if ec2_sshpubk and not os.path.isfile(ec2_sshpubk):
-    sys.exit('Error: file %s doesnt exist' % ec2_sshpubk)
-
-  instance = start(IMG, ec2_keypair)
-
+  ec2_keypair, ssh_privk, branch, ec2_image = args
   try:
-    run(ec2_sshpubk, instance.dns_name, (
-        'wget https://raw.github.com/jjo/bogomon/%s/setup/ec2-setup.sh;'
-        'bash -x ec2-setup.sh %s') % (branch, branch))
+    ec2_instance = EC2bogo(ec2_image, ec2_keypair, ssh_privk, branch)
+    ec2_instance.start()
+    ec2_instance.run((
+          'wget https://raw.github.com/jjo/bogomon/%s/setup/ec2-setup.sh;'
+          'bash -x ec2-setup.sh %s') % (branch, branch))
+    print ('Hopefully ready at:\n'
+           'http://{0}:{1}/stats.html\ninstance: {2}'.format(
+             ec2_instance.instance.dns_name, BOGOMON_PORT,
+             ec2_instance.instance))
   except SystemExit:
-    print "Failed ssh setup, destroying {0}".format(instance)
-    instance.stop()
     sys.exit(1)
-
-  print "Hopefully ready at:\nhttp://{0}:{1}/stats.html\ninstance: {2}".format(
-      instance.dns_name, BOGOMON_PORT, instance)
 
 if __name__ == '__main__':
   main()
