9751027cc2ce3bd61d34db311c3b19926ccf9e62
JuanJo Ciarlante <juanjosec@gmail.com>
2011-11-12 23:33:28 -0800

    ec2_setup.py: exception/doc-ize, ec2-setup.sh: better sudo mgmt

--- setup/ec2-setup.sh
+++ setup/ec2-setup.sh
@@ -1,15 +1,26 @@
 PKGS="nginx python-flup python-rrdtool rrdtool spawn-fcgi"
+# Packages setup
+sudo bash -xe || exit 1 << EOF
+  apt-get update
+  apt-get -y install $PKGS
+EOF
 
-sudo apt-get -y install $PKGS
-id -u bogomon || sudo useradd bogomon || exit 1
-sudo mkdir -p /usr/lib/bogomon /var/lib/bogomon /var/run/bogomon
-REPO=https://raw.github.com/jjo/bogomon/master
-for f in /etc/nginx/sites-available/bogomon /usr/lib/bogomon/bogomon.fcgi.py /etc/init.d/spawn-fcgi;do
-	sudo wget -O $f $REPO$f || exit 1
-	case $f in /etc/init.d/*|/usr/lib/*) sudo chmod +x $f;;esac
-done
-sudo ln -sf ../sites-available/bogomon /etc/nginx/sites-enabled/bogomon
-sudo chown -R bogomon:bogomon /var/lib/bogomon
-sudo /etc/init.d/spawn-fcgi restart
-sudo /etc/init.d/nginx configtest || exit 1
-sudo /etc/init.d/nginx status && sudo /etc/init.d/nginx start || sudo /etc/init.d/nginx restart
+# Create 'bogomon' user, create and directories
+sudo bash -xe || exit 2 << EOF
+  id -u bogomon || useradd bogomon || exit 1
+  mkdir -p /usr/lib/bogomon /var/lib/bogomon /var/run/bogomon
+  REPO=https://raw.github.com/jjo/bogomon/master
+  for f in /etc/nginx/sites-available/bogomon /usr/lib/bogomon/bogomon.fcgi.py /etc/init.d/spawn-fcgi;do
+    wget -O $f $REPO$f || exit 1
+    case $f in /etc/init.d/*|/usr/lib/*) chmod +x $f;;esac
+  done
+  ln -sf ../sites-available/bogomon /etc/nginx/sites-enabled/bogomon
+  chown -R bogomon:bogomon /var/lib/bogomon
+EOF
+
+# Start the services
+sudo bash -xe || exit 3 << EOF
+  /etc/init.d/spawn-fcgi restart
+  /etc/init.d/nginx configtest
+  /etc/init.d/nginx status && sudo /etc/init.d/nginx start || sudo /etc/init.d/nginx restart
+EOF
--- setup/ec2_create.py
+++ setup/ec2_create.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
-""" jjo is tired (1am) """
-import boto, os, time, telnetlib, socket
+""" Spawn an EC2 instance, populating according with:
+    https://raw.github.com/jjo/bogomon/<branch>/setup/ec2-setup.sh
+    , which is essentially an ubuntu 32bits
+    + nginx
+    + spawn-fcgi running bogomon.fcgi.py, which does:
+      - periodic cpu usage collection into RRD file
+      - generate html+png stats
+
+"""
+import boto, os, time, telnetlib, socket, sys
 
 IMG = 'ami-29f43840' ## ubuntu 11.10 32bit
-KEY = 'ec2-keypair'  ## ssh pubkey to be planted into the instance
+BOGOMON_PORT = 8000  ## only used for the final "informational" message
+BRANCH = 'master'    ## potentially allows for e.g. 'staging', 'experimental'
 
-def start():
-  """ jjo is tired (1am) """
-  print 'Starting EC2 with image {0}'.format(IMG)
+def start(ec2_img, ec2_keypair):
+  """ Connect to EC2, spawn an instance from ec2_img, attaching
+      ec2_keypair to it """
+  print 'Starting EC2 with image {0}'.format(ec2_img)
   ec2_conn = boto.connect_ec2()
-  images = ec2_conn.get_all_images(image_ids=['ami-29f43840'])
-  reserv = images[0].run(1, 1, 'ec2-keypair')
+  images = ec2_conn.get_all_images(image_ids=[ec2_img])
+  reserv = images[0].run(1, 1, ec2_keypair)
   instance = reserv.instances[0]
   time.sleep(10)
   while not instance.update() == 'running':
@@ -18,21 +28,52 @@ def start():
   while not instance.dns_name:
     instance.update()
     time.sleep(1)
-  print 'Started the hostname: {0}'.format(instance.dns_name)
-  return instance.dns_name
+  print 'Started the hostname: {0}, instance: {1}'.format(
+      instance.dns_name, instance)
+  return instance
 
-def run(host, cmd):
-  """ jjo is tired (1am) """
-  i = 30
-  while i > 0:
-    i = i - 1
+def run(host, ssh_pubk, cmd):
+  """ Run: ssh ubunbu@... at just created host, to complete its setup """
+  for _ in xrange(30):
     try:
       telnetlib.Telnet(host, 22)
       break
     except socket.error:
       time.sleep(5)
-  os.system('ssh ubuntu@%s "%s"' % (host, cmd))
+  if ssh_pubk:
+    ssh_pubk = "-i %s" % ssh_pubk
+  retval = os.system('ssh %s ubuntu@%s "%s"' % (ssh_pubk, host, cmd))
+  if retval != 0:
+    raise SystemExit
 
-HOST = start()
-run(HOST, "wget -O- https://raw.github.com/jjo/bogomon/master/setup/ec2-setup.sh |bash -x")
-print "Ready at:\nhttp://%s:%d/stats.html \o/" % (HOST, 8000)
+def main():
+  """ main(), what else """
+  # Override default args with argv[1:] ,
+  # also IMO easier for validation instead of len(sys.argv)
+  args = [None, None]
+  for i, val in enumerate(sys.argv[1:]):
+    args[i] = val
+  if args[0] is None:
+    sys.exit('Usage: %s <ec2_keypair> [ssh_priv_key_filepath]\n')
+
+  ec2_keypair, ec2_sshpubk = args
+  if ec2_sshpubk and not os.path.isfile(ec2_sshpubk):
+    sys.exit('Error: file %s doesnt exist' % ec2_sshpubk)
+
+  instance = start(IMG, ec2_keypair)
+
+  try:
+    run(instance.dns_name, ec2_sshpubk, (
+        'wget -O- https://raw.github.com/'
+        'jjo/bogomon/%s/setup/ec2-setup.sh |bash -x') % (BRANCH))
+  except SystemExit:
+    print "Failed ssh setup, destroying {0}".format(instance)
+    instance.stop()
+    sys.exit(1)
+
+  print "Hopefully ready at:\nhttp://{0}:{1}/stats.html\ninstance: {2}".format(
+      instance.dns_name, BOGOMON_PORT, instance)
+
+if __name__ == '__main__':
+  main()
+
